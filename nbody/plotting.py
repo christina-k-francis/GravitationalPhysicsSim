32ac29f1136e118c9e9efb17a0e22872f6b58975
Christina Francis <67291164+christina-k-francis@users.noreply.github.com>
2023-07-11 17:25:05 -0400

    Update plotting.py
    Made the scatter plot more colorful

--- nbody/plotting.py
+++ nbody/plotting.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
-
 def plotObjects(objects):
     '''Draw a matplotlib chart of our
     objects. `objects` argument is an Nx3 matrix of (x,y,mass) coordinates'''
-    plt.figure(figsize=(12,6))
-    plt.scatter(objects[:,0],objects[:,1], s=objects[:,2], alpha=0.5)
+    plt.figure(figsize=(5,4))
+    plt.scatter(objects[:,0],objects[:,1], s=objects[:,2], alpha=0.75,
+                c=np.arange(len(objects)), cmap='nipy_spectral')
     plt.show()
     
